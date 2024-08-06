716c679ad0442576fc84694466839844388246f1
Jung Hun Phee <pjh961018@naver.com>
2024-08-07 00:26:59 +0900

    Update application.py

--- codes/application.py
+++ codes/application.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import pickle
 import numpy as np
 import random
@@ -14,25 +13,26 @@ from collections.abc import Iterable
 
 from copy import deepcopy
 
-from counting import *
-from utils import *
-
+from codes.counting import *
+from codes.utils import *
 
 class application:
-    def __init__(self,  data, bin, n_tail,  initial=0, last=0, device = 'cpu', parallel = False, timer=False):
+    def __init__(self,  data, bin, n_tail,  initial=0, last=0, device = 'cpu', parallel = False, timer=False, print_computing_time=False):
         self.data = data
         self.n_tail = n_tail
         self.device = device
         self.parallel = parallel
         self.initial = initial
         self.last = last
+        assert timer - print_computing_time > -1, "Training time cannot be printed if timer is False."
+
         
         if self.is_list_of_sequences(data):
-            G = multig4ts(data, bin=bin, n_tail = n_tail, initial=initial, last=last, device=device, parallel=parallel,timer=timer)
+            G = multig4ts(data, bin=bin, n_tail = n_tail, initial=initial, last=last, device=device, parallel=parallel,timer=timer, print_computing_time=print_computing_time)
             self.edges = G.edges
         else:
             G = g4ts(data, bin=bin,  initial=initial, last=last, device=device, parallel=parallel, timer=timer)
-            self.edges = G.compute(n_tail=self.n_tail)
+            self.edges = G.compute(n_tail=self.n_tail, print_computing_time = print_computing_time)
 
         self.G = G
         self.bin = G.bin
@@ -40,7 +40,7 @@ class application:
         self.lastnodes = G.lastnodes
         self.newnodes = np.array([])
         if timer:
-            self.timer = G.timer
+            self.timer = G.duration
         
     def is_list_of_sequences(self,dataset):
         # Check if the dataset itself is a list
@@ -111,7 +111,7 @@ class application:
         self.newnodes = newnodes
         return newnodes.astype(int)
     
-    def generate(self, n, initialnodes, how='average'):
+    def generate(self, n, initialnodes, how='average', inference_time=False):
         """
         It generates new data of length n.
         It converts a seq of nodes into real data list.
@@ -121,7 +121,10 @@ class application:
         #Now generate into continuous ones. 
         newnodes = self.newnodes
         if len(newnodes) == 0:
+            inferece_node_start = time.perf_counter()
             newnodes = self.generate_newnodes(n=n, initialnodes=initialnodes)
+            inferece_node_end = time.perf_counter()
+            inference_node_time = inferece_node_end - inferece_node_start
         gendata = np.empty(len(newnodes))
         bins = self.bin
         bins.append(self.G.M)
@@ -132,8 +135,14 @@ class application:
             for i in range(len(gendata)):
                 gendata[i] = sum((bins[int(newnodes[i])],bins[int(newnodes[i]+1)]))/2
         elif how=='uniform':
+            inference_start = time.perf_counter()
             for i in range(len(gendata)):
                 gendata[i] = random.uniform(bins[int(newnodes[i])],bins[int(newnodes[i]+1)])
+            inference_end = time.perf_counter()
+        inference_timer = inference_end - inference_start
+        inference_timer = inference_timer + inference_node_time
+        if inference_time is True:
+            print(f"Inference Time for time length {len(gendata)} is {inference_timer}")
         elif how=='median':
             print('You need to write code')
             #for i in range(len(gendata)):
@@ -152,22 +161,6 @@ class application:
         self.G.make_each_vertex_distinct()
         edges = self.G.edges
         
-        
-        
-        
-        
-    
-   # def plot(self, training, target, color, )
-    
-    #def evaluate(self, validation_data, metric,device = 'cpu', parallel = False ):
-        
-    
-   # def bridge(self, n, initial, end, device = 'cpu', parallel = False):
-        
-        
-        
-            
-    
     
 
 
@@ -197,6 +190,10 @@ def simulate_markov_bridge(P, initial_state, final_state, n_steps):
     return path
         
         
+
+
+
+
 
 
 
